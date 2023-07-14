4dab4b943c88f20f5d70b742b85e27aab79bae70
haoliang <haoliang0.1.2@gmail.com>
2023-07-14 14:43:13 +0800

    sync with upstream

--- lua/tui/menu.lua
+++ lua/tui/menu.lua
@@ -3,17 +3,17 @@
 --design
 --* keys are subset of [a-z]
 --  * order is predicatable
---* singleton, and reuses a dedicated buffer
+--* each menu have its own buffer, multiple menus can appear at the same time
 --* compact window layout, less eyes movement
 --  * respects cursor position
 --  * respects #choices and max(#each-choice)
 --* dont reset window options and buffer lines initiatively
 
-local jelly = require("infra.jellyfish")("tui.menu")
-local fn = require("infra.fn")
 local bufrename = require("infra.bufrename")
-local prefer = require("infra.prefer")
+local fn = require("infra.fn")
+local jelly = require("infra.jellyfish")("tui.menu", "debug")
 local bufmap = require("infra.keymap.buffer")
+local prefer = require("infra.prefer")
 
 local api = vim.api
 
@@ -35,121 +35,112 @@ do
   function Keys.iter() return fn.iter(list) end
 end
 
-local state = { bufnr = nil, winid = nil, using = false, entries = nil, choice = nil }
+local next_canvas_id
 do
-  ---@return number
-  function state:prepare_buffer()
-    if state.bufnr ~= nil then
-      assert(api.nvim_buf_is_valid(state.bufnr), "unreachable")
-      return state.bufnr
-    end
+  local count = 0
+  function next_canvas_id()
+    count = count + 1
+    return count
+  end
+end
 
-    state.bufnr = api.nvim_create_buf(false, true)
-    bufrename(state.bufnr, "tui://menu")
+---@param entries string[]
+---@param formatter fun(entry: string):string
+---@param prompt? string
+---@param callback fun(entry: string?, index: number?)
+local function main(entries, formatter, prompt, callback)
+  local lines = {}
+  do
+    local key_iter = Keys.iter()
+    for _, ent in ipairs(entries) do
+      local key = assert(key_iter(), "no more lhs is available")
+      local line = string.format(" %s. %s", key, formatter(ent))
+      table.insert(lines, line)
+    end
+  end
+
+  local win_height, win_width
+  do
+    local line_max = 0
+    for _, line in ipairs(lines) do
+      if #line > line_max then line_max = #line end
+    end
+    win_height = #lines
+    win_width = line_max + 1
+    if prompt ~= nil then
+      -- 1->winbar
+      win_height = win_height + 1
+      -- 1->留白
+      win_width = math.max(win_width, math.min(#prompt, 20))
+    end
+  end
+
+  local canvas = { entries = entries, callback = callback, choice = nil, bufnr = nil, winid = nil }
+
+  do -- setup buf
+    canvas.bufnr = api.nvim_create_buf(false, true)
+
+    prefer.bo(canvas.bufnr, "bufhidden", "wipe")
+    bufrename(canvas.bufnr, string.format("tui://menu#%d", next_canvas_id()))
+
     for key in Keys.iter() do
       -- unable to use infra.keymap.buffer here
-      api.nvim_buf_set_keymap(state.bufnr, "n", key, "", {
+      api.nvim_buf_set_keymap(canvas.bufnr, "n", key, "", {
         noremap = true,
         nowait = true,
         callback = function()
           local n = assert(Keys.index(key), "unreachable: invalid key")
           -- not a present entry, do nothing
-          if n > #self.entries then return end
-          state.choice = n
-          api.nvim_win_close(state.winid, false)
+          if n > api.nvim_buf_line_count(canvas.bufnr) then return jelly.info("no such option: %s", key) end
+          canvas.choice = n
+          api.nvim_win_close(canvas.winid, false)
         end,
       })
     end
 
-    local bm = bufmap.wraps(state.bufnr)
+    local bm = bufmap.wraps(canvas.bufnr)
     for key in fn.iter({ "q", "<esc>", "<c-[>", "<c-]>" }) do
-      bm.n(key, function() api.nvim_win_close(state.winid, false) end)
+      bm.n(key, function() api.nvim_win_close(canvas.winid, false) end)
     end
 
-    return state.bufnr
+    --* `:q` triggers winclosed
+    --* ignore winleave, which is hard to handle
+    api.nvim_create_autocmd("winclosed", {
+      buffer = canvas.bufnr,
+      once = true,
+      callback = function()
+        local choice = canvas.choice
+        canvas.callback(canvas.entries[choice], choice)
+      end,
+    })
   end
 
-  function state:cleanup()
-    state.winid = nil
-    state.using = false
-    state.entries = nil
-    state.choice = nil
+  do -- render the content and display
+    api.nvim_buf_set_lines(canvas.bufnr, 0, -1, false, lines)
+    local winid = api.nvim_open_win(canvas.bufnr, true, { relative = "cursor", row = 1, col = 0, width = win_width, height = win_height, style = "minimal" })
+    canvas.winid = winid
+    prefer.wo(winid, "winbar", prompt or "")
   end
+
+  -- there is no easy way to hide the cursor, let it be there
 end
 
 ---@param entries string[]
 ---@param opts {prompt: string?, format_item: fun(entry: string): (string), kind: string?}
 ---@param callback fun(entry: string?, index: number?)
 return function(entries, opts, callback)
+  opts = opts or {}
+
   local formatter
   do
     if opts.kind ~= nil then
       formatter = assert(opts.format_item, "opts.format_item is required for custom opts.kind")
     else
-      -- stylua: ignore
       formatter = opts.format_item or function(ent) return ent end
     end
   end
+
   local prompt = opts.prompt
-  local has_prompt = prompt ~= nil
 
-  if state.using then return jelly.err("a menu is just displaying, complete it to continue!") end
-  assert(state.winid == nil and state.choice == nil, "unreachable: dirty state")
-  state.using = true
-
-  state:prepare_buffer()
-  state.entries = entries
-
-  local win_height
-  local win_width
-  do
-    local lines = {}
-    local line_max = 0
-    local key_iter = Keys.iter()
-    for ent in fn.iter(state.entries) do
-      local key = assert(key_iter(), "no more lhs is available")
-      local line = string.format(" %s. %s", key, formatter(ent))
-      table.insert(lines, line)
-      if #line > line_max then line_max = #line end
-    end
-    api.nvim_buf_set_lines(state.bufnr, 0, -1, false, lines)
-    win_height = #lines
-    -- 1->winbar
-    if has_prompt then win_height = win_height + 1 end
-    -- 1->留白
-    win_width = line_max + 1
-    if has_prompt then win_width = math.max(win_width, math.min(#prompt, 20)) end
-  end
-
-  do
-    -- stylua: ignore
-    state.winid = api.nvim_open_win(state.bufnr, true, {
-      relative = "cursor", row = 1, col = 0, width = win_width, height = win_height,
-      style = "minimal",
-    })
-    prefer.wo(state.winid, "winbar", prompt or "")
-
-    api.nvim_create_autocmd("winleave", {
-      buffer = state.bufnr,
-      once = true,
-      nested = true, -- so that we can trigger the winclosed event
-      callback = function() api.nvim_win_close(state.winid, false) end,
-    })
-
-    api.nvim_create_autocmd("winclosed", {
-      buffer = state.bufnr,
-      once = true,
-      callback = function()
-        local choice = state.choice
-        state:cleanup()
-        if choice ~= nil then
-          callback(entries[choice], choice)
-        else
-          callback(nil, nil)
-        end
-      end,
-    })
-  end
-
-  -- there is no easy way to hide the cursor, let it be there
+  main(entries, formatter, prompt, callback)
 end
